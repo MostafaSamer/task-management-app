33c4765f05556e6f057d38628a98f5e91e630ad3
root <root@mostafa-pc>
2023-11-22 01:20:00 +0200

    add style and fonts

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router';
 
+import './index.scss';
+
 const Root = () => {
    return <React.StrictMode>
       <BrowserRouter>
