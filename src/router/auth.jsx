44ea60e0f7df74a373d9c434e598058ee73ef6dc
Mostafa Samir <mostafasamer1999@hotmail.com>
2023-11-25 14:21:33 +0200

    add route guard

--- src/router/auth.jsx
+++ src/router/auth.jsx
@@ -1,8 +1,14 @@
 import { Route } from 'react-router-dom';
 import DefaultLayout from 'layouts/DefaultLayout';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Users from 'redux/users';
 
 const AuthRoute = ({ path, component: Component }) => {
-    // check if user is logged in
+    const history = useHistory();
+    const user = useSelector(Users.selectors.getCurrentUser);
+    if (user) history.push('/');
+
     return <DefaultLayout>
         <Route exact path={path} component={Component} />
     </DefaultLayout>;
--- src/router/user.jsx
+++ src/router/user.jsx
@@ -1,11 +1,17 @@
 import { Route } from 'react-router-dom';
 import DefaultLayout from 'layouts/DefaultLayout';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Users from 'redux/users';
 
-const SponsorRoute = ({ path, component: Component }) => {
-    // check if user is logged in
+const UserRoute = ({ path, component: Component }) => {
+    const history = useHistory();
+    const user = useSelector(Users.selectors.getCurrentUser);
+    if (!user) history.push('/signin');
+
     return <DefaultLayout>
         <Route exact path={path} component={Component} />
     </DefaultLayout>;
 };
 
-export default SponsorRoute;
+export default UserRoute;
